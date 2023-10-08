703690131de79107e671b2bbed1eec6a077cfd14
autbdu42822 <144507542+autbdu42822@users.noreply.github.com>
2023-10-08 16:51:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-class BankAccount:
+def LinearSearchProduct(productlist,targetproduct):
+  play=[]
+  for index, products in enumerate(productlist):
+    if products == targetproduct:
+      play.append(index)
+  return play
+products=["shoes","boot","loafr","shoes","sandal","shoes","shoes","sandal"]
+target="shoes"
+result=LinearSearchProduct(products,target)
+print(result)
 
-  def __init__(self):
-    self.balance = 0
-    print("Bank account details ")
-
-  def deposit(self):
-    amount = float(input("enter the amount:"))
-    self.balance += amount
-    print("amount is deposit in your account ", amount)
-
-  def withdraw(self):
-    amount = float(input("enter your amount:"))
-    if self.balance >= amount:
-      self.balance -= amount
-      print("your withdraw:", amount)
-    else:
-      print("you don't have enough money")
-
-  def display(self):
-    print("available balance:", self.balance)
-
-
-s = BankAccount()
-s.deposit()
-s.withdraw()
-s.display()
